97006c3941dfbbca014c2e30e495213e5d06202d
Jared3256 <odhiambojared566@gmail.com>
2025-06-16 14:54:43 +0300

    finished the admin unit managemement = approving , rejecting, reseting

--- src/Routes/unit_registration/unit.registration.route.js
+++ src/Routes/unit_registration/unit.registration.route.js
@@ -3,7 +3,12 @@ import createUnitRegistrationController from "../../controller/units_registratio
 
 const unitRegistrationRouter = express.Router();
 
+// Student ony routes
 unitRegistrationRouter.post("/student/create", createUnitRegistrationController.register)
 unitRegistrationRouter.get("/student/:id/list", createUnitRegistrationController.student_list)
 unitRegistrationRouter.put("/student/:id/cancel", createUnitRegistrationController.student_cancel)
+
+// Admin Only routes
+unitRegistrationRouter.get("/admin/:id/list", createUnitRegistrationController.admin_list)
+unitRegistrationRouter.put("/:regId/admin/:adminId/approve", createUnitRegistrationController.admin_approve)
 export default unitRegistrationRouter;
--- src/controller/units_registration/createUnitRegistrationController.js
+++ src/controller/units_registration/createUnitRegistrationController.js
@@ -1,13 +1,21 @@
 import RegisterUnit from "./register.js";
 import ListStudentRegisteredUnits from "./student.unit.list.js";
 import CancelUnitRegistration from "./cancel.js";
+import AdminUnitList from "./admin.unit.list.js";
+import ApproveUnitRegistration from "./approve.js";
 
 const cancelUnitRegistrationController = () => {
     const methods = {}
 
     methods.register = (req, res) => RegisterUnit(req, res)
     methods.student_list = (req, res) => ListStudentRegisteredUnits(req, res)
-    methods.student_cancel = (req, res)=> CancelUnitRegistration(req, res)
+    methods.student_cancel = (req, res) => CancelUnitRegistration(req, res)
+
+    // Admin Only methods
+    methods.admin_list = (req, res) => AdminUnitList(req, res)
+    methods.admin_approve = (req,res) => ApproveUnitRegistration(req, res)
+
+
     return methods
 }
 
