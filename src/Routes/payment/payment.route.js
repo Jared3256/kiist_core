dc4e4620dfca4786ce7b9a7c800d01a0f0e97872
Jared3256 <odhiambojared566@gmail.com>
2025-06-26 21:26:03 +0300

    modified callback url

--- src/Routes/payment/payment.route.js
+++ src/Routes/payment/payment.route.js
@@ -3,7 +3,7 @@ import createPAYController from "../../controller/payment/createPAYController.js
 
 const paymentRouter = express.Router();
 
-paymentRouter.post("/daraja_callback", createPAYController.callback);
+paymentRouter.post("/back", createPAYController.callback);
 paymentRouter.get("/authorization", createPAYController.authorization);
 paymentRouter.post("/stkpush", createPAYController.stkpush);
 paymentRouter.post("/validation", createPAYController.validation)
