0b67c9672dcc0914994e475731bf367ab879b5c8
Jared3256 <odhiambojared566@gmail.com>
2025-06-28 21:10:59 +0300

    changed the private key path

--- src/controller/payment/Jenga/geneateSignature.js
+++ src/controller/payment/Jenga/geneateSignature.js
@@ -1,22 +1,21 @@
-import fs from "fs";
 import crypto from "crypto";
-import path from "path";
 import asyncHandler from "express-async-handler"
+import system_data from "../../../config/environment/env.constants.js";
 
 
 const GenerateJengaSignature = asyncHandler(async (message) => {
-    const privateKeyPath = path.resolve("privatekey.pem");
+
 
     const messageBuffer = Buffer.from(message, "utf-8");
 
-    const privateKeyPem = fs.readFileSync(privateKeyPath, "utf-8");
+    const privateKeyPem = Buffer.from(system_data.JENGA_PRIVATE_KEY, "base64").toString("utf-8");
 
     const sign = crypto.createSign("RSA-SHA256");
     sign.update(messageBuffer);
     sign.end();
 
     const signature = sign.sign(privateKeyPem, "base64");
-    
+
     return signature
 })
 export default GenerateJengaSignature;
