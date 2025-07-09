29fac3e84fbe5b6ecbba7db595b79a637db02b2c
Jared3256 <odhiambojared566@gmail.com>
2025-07-09 21:20:01 +0300

    whitelisted kiist.ac.ke

--- src/config/cors/allowedOrigins.js
+++ src/config/cors/allowedOrigins.js
@@ -2,6 +2,7 @@ export const allowedOrigins = [
     "http://localhost:3000",
     "http://localhost:5173",
     "https://kiist.vercel.app",
+    "https://kiist.ac.ke",
     "196.201.214.200",
     "196.201.214.206",
     "196.201.213.114",
